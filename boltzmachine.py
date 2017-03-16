9fc3f3843e15e726db175b95e5b66999de46471c
Spstolar <spstolar@math.uh.edu>
2017-03-15 20:10:15 -0500

    Build the Boltzmann Machine class. Define state and weights.

--- boltzmachine.py
+++ boltzmachine.py
@@ -1,7 +1,17 @@
 import numpy as np
 
-practice_matrix = np.zeros((3,5))
 
-practice_matrix[2,2] = 7
+class BoltzmannMachine(object):
+    def __init__(self, input_size, hidden_size, output_size):
+        self.total_nodes = input_size + hidden_size + output_size
+        self.state = np.random.randint(0, 2, self.total_nodes, dtype=bool)  # Begin with a random Boolean State.
+        self.weights = np.random.normal(0, 1, size=(self.total_nodes,self.total_nodes))  # Random weights from N(0,1).
+        self.disconnect = np.random.randint(0, 2, (self.total_nodes,self.total_nodes))  # Choose weights to zero out.
+        self.weights = self.disconnect * self.weights  # Zero out the weights chosen by disconnect. Point-wise mult.
 
-print practice_matrix
+    def print_current_state(self):
+        print self.state
+
+BM = BoltzmannMachine(2, 4, 2)
+BM.print_current_state()
+
