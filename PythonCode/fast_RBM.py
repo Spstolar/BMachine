e0b1781c3db74949cf14b03de2828745fcd90ed6
Simon <spstolar@math.uh.edu>
2017-03-30 14:28:12 -0500

    Begin the training algorithm and delete the unwanted weights.

--- PythonCode/fast_RBM.py
+++ PythonCode/fast_RBM.py
@@ -7,9 +7,17 @@ def sigmoid(input):
 class BoltzmannMachine(object):
     def __init__(self, input_size, hidden_size, output_size):
         self.total_nodes = input_size + hidden_size + output_size
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        self.hidden_ind = input_size
+        self.out_ind = input_size + output_size
+        
         self.state = np.random.randint(0, 2, self.total_nodes, dtype=int)  # Begin with a random 0-1 draw.
         self.state = (self.state - .5) * 2  # Convert to -1, +1 state.
         self.weights = self.create_random_weights()
+        self.correct_weights()
+        
         self.history = self.state
         self.sweeps = 1000
         self.stabilization = np.zeros((self.sweeps, self.total_nodes))
@@ -43,6 +51,12 @@ class BoltzmannMachine(object):
         result = 2*(coin_flip - .5)  # Convert biased coin flip to -1 or 1.
         # print result
         self.state[node] = result
+    
+    def mle_update(self,node):
+        if self.conditional_prob(node) > .5 :
+            self.state[node] = 1    
+        else:
+            self.state[node] = -1
 
     def run_machine(self, sweep_num, stabilized=0):
         visit_list = np.arange(self.total_nodes)  # The array [0 1 ... n-1].
@@ -76,10 +90,47 @@ class BoltzmannMachine(object):
         weights = np.triu(weights, k=1)  # discard lower diagonal terms (and the diagonal to avoid self-connections)
         weights = weights + weights.T  # make the weights symmetric
         return weights
+    
+    def correct_weights(self):
+        self.weights[:self.hidden_ind, :self.hidden_ind] = 0
+        self.weights[-self.output_size:,-self.output_size:] = 0
+        self.weights[self.hidden_ind:,:self.hidden_ind] = 0
+        self.weights[:self.hidden_ind, self.hidden_ind:] = 0
+        
 
     def empirical_mean(self):
         return np.mean(self.history, axis=0)
-
+        
+    def clamped_run(self, in_state, out_state, sweep_num=1):
+    	# Runs the machine while forcing the input nodes and output nodes to stay the same. 
+    	# Only the hidden nodes will change.
+    	# Can return final hidden configuration.
+    	# Can return the coactivity 
+    	# Updates the hidden states.
+        visit_list = np.arange(self.size)  # The array [0 1 ... n-1].
+        for sweep in range(sweep_num):
+        np.random.shuffle(visit_list)  # Shuffle the array [0 1 ... n-1].
+        for node_num in range(self.hidden_size):
+        node_to_update = visit_list[node_num + self.input_size]
+        self.update(node_to_update)
+		
+    def clamped_run_mle(self, in_state, out_state):
+        for node_num in range(self.hidden_size):
+            self.mle_update(node_num + self.input_size)
+                    
+    def training(self, example_set, batch_size, num_batches, iterations):
+        for it in range(iterations):
+            # shuffle data
+            for b in range(num_batches):
+                batch_process(batch)
+                            
+    def batch_process(self, batch, batch_size):
+        for ex in range(batch_size):	
+            clamped_run_mle(batch[ex, :self.hidden_ind], batch[ex, self.out_ind:])
+                
+    def coactivity(self):
+        return np.outer(self.state, self.state) 
+        
 
 start_time = time.time()
 
