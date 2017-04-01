6692fa99ba2991ac221d77a459aef4d22a8414e4
Spstolar <spstolar@math.uh.edu>
2017-03-31 22:21:55 -0500

    Creates a simple histogram with the normal distribution sampling.

--- PythonCode/hist_maker.py
+++ PythonCode/hist_maker.py
@@ -1,3 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+mu, sigma = 100, 15
+x = mu + sigma * np.random.randn(1000)
+
+n, bins, patches = plt.hist(x, 50, normed=1, facecolor='green', alpha=0.75)
+
+plt.xlabel('Hey')
+plt.ylabel('There')
+plt.title(r'$\int_{\Omega} f(\omega) d\omega$')
+plt.axis([40,160,0,0.03])
+plt.grid(True)
+
+plt.show()
+
+plt.savefig('example_hist.png')
