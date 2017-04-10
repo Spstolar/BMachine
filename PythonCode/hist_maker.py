0fb8a60a192b7025a9f4aea7ab62e8378b06cce2
adrian <adrian@math.uh.edu>
2017-04-09 21:55:03 -0500

    correct x-axis range in hist_maker.py

--- PythonCode/hist_maker.py
+++ PythonCode/hist_maker.py
@@ -25,7 +25,7 @@ n, bins, patches = plt.hist(x, 31, normed=0, histtype='bar',facecolor='green', a
 plt.xlabel('Energy Value')
 plt.ylabel('Number of Sweeps')
 plt.title('Energy Values for Post-Stabilization Sweeps - Large Weights')
-plt.axis([30,70,0,30])  # This is for the large weights.
+plt.axis([40,80,0,30])  # This is for the large weights.
 # plt.axis([-3,4,0,30])  # This is for the small weights.
 plt.grid(True)
 plt.grid(color='b', linestyle='-', linewidth=.1,alpha=.3)
